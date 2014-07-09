9f0a74e2c44a4cf57e900a657090aef84154f516
boffley@gmail.com <boffley@gmail.com@a24f1770-fd37-cddf-c9c3-e7d3d9b82b57>
2014-07-09 21:08:39 +0000

    added image files for cards and mini versions for showing during gameplay added single player setup screen and options for number of AI players as well as turning either beginner mode or random card selection modes on

--- trunk/Tanto Cuore/Tanto Cuore/Game1.cs
+++ trunk/Tanto Cuore/Tanto Cuore/Game1.cs
@@ -19,7 +19,7 @@ namespace Tanto_Cuore
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         int screenNumber = 0; //0 main screen, 1 single player setup, 2 multiplayer setup, 3 settings, 4 gamplay screen, 5 results screen
-        int currentMainMenuOption = 0; //0 single player setup, 1 multiplayer setup, 2 settings, 3 exit game.
+        int currentMainMenuOption =0 ; //0 single player setup, 1 multiplayer setup, 2 settings, 3 exit game.
         const int mainMenu = 0;
         const int singlePlayerSetup = 1;
         const int multiplayerSetup = 2;
@@ -29,6 +29,16 @@ namespace Tanto_Cuore
         int numberOfMainMenuOptions = 4;
         SpriteFont font;
         String[] mainMenuOptionsStrings;
+        int currentSinglePlayerSetupMenuOption = 0; //0 beginner mode, 1 random mode, 2 edit general maids, 3 number of ai, 4 start, 5 return.
+        int numberOfSinglePlayerSetupMenuOptions = 6;
+        String[] singlePlayerSetupMenuOptionsStrings;
+        bool beginnerMode = true;
+        bool randomMode = false;
+        PlayArea playArea;
+        int numberOfAIPlayers = 1;
+
+        Texture2D[] cardPicturesFull;
+        Texture2D[] cardPicturesMini;
 
         KeyboardState keyboardOld;
         MouseState mouseOld;
@@ -52,8 +62,21 @@ namespace Tanto_Cuore
             mouseOld = new MouseState();
             keyboardOld = new KeyboardState();
             mainMenuOptionsStrings = new String[numberOfMainMenuOptions];
+            mainMenuOptionsStrings[singlePlayerSetup - 1] = "Singleplayer Setup";
+            mainMenuOptionsStrings[multiplayerSetup - 1] = "Multiplayer Setup";
+            mainMenuOptionsStrings[settings - 1] = "Settings";
+            mainMenuOptionsStrings[3] = "End Game";
+            singlePlayerSetupMenuOptionsStrings = new String[numberOfSinglePlayerSetupMenuOptions];
+            singlePlayerSetupMenuOptionsStrings[0] = "Beginner Mode";
+            singlePlayerSetupMenuOptionsStrings[1] = "Random Mode";
+            singlePlayerSetupMenuOptionsStrings[2] = "Edit General Maids";
+            singlePlayerSetupMenuOptionsStrings[3] = "Number of AI Players: ";
+            singlePlayerSetupMenuOptionsStrings[4] = "Start Single Player";
+            singlePlayerSetupMenuOptionsStrings[5] = "Return";
+            cardPicturesFull = new Texture2D[33];
+            cardPicturesMini = new Texture2D[33];
+            PlayArea playArea = new PlayArea();
             base.Initialize();
-            //PlayArea playArea = new PlayArea();
             //List<Card> generalMaids = new List<Card>();
             //for (int index = 0; index < 15; index++)
             //{
@@ -76,11 +99,12 @@ namespace Tanto_Cuore
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
-            mainMenuOptionsStrings[singlePlayerSetup - 1] = "Singleplayer Setup";
-            mainMenuOptionsStrings[multiplayerSetup - 1] = "Multiplayer Setup";
-            mainMenuOptionsStrings[settings - 1] = "Settings";
-            mainMenuOptionsStrings[3] = "End Game";
             font = this.Content.Load<SpriteFont>("menuFont");
+            for (int index = 0; index < 33; index++)
+            {
+                cardPicturesFull[index] = this.Content.Load<Texture2D>((index + 1) + "");
+                cardPicturesMini[index] = this.Content.Load<Texture2D>((index + 1) + "-mini");
+            }
         }
 
         /// <summary>
@@ -114,13 +138,94 @@ namespace Tanto_Cuore
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-            if (screenNumber == mainMenu)
-            {
-                drawMainMenu(gameTime);
+            switch(screenNumber){
+                case mainMenu:
+                    drawMainMenu(gameTime);
+                    break;
+                case singlePlayerSetup:
+                    drawSinglePlayerSetupMenuOptionsStrings(gameTime);
+                    break;
+                case multiplayerSetup:
+                    break;
+                case gameplayScreen:
+                    break;
+                case settings:
+                    break;
+                case resultsScreen:
+                    break;
             }
             // TODO: Add your drawing code here
 
             base.Draw(gameTime);
+        }
+
+        private void drawSinglePlayerSetupMenuOptionsStrings(GameTime gameTime)
+        {
+            spriteBatch.Begin();
+            //spriteBatch.Draw(mainMenu, Vector2.Zero, Color.White);
+            //spriteBatch.Draw(mainMenu, new Vector2(graphics.GraphicsDevice.Viewport.Width / 2 - mainMenu.Width / 2, 100), Color.White);
+            spriteBatch.DrawString(font, "Single Player Setup Menu", new Vector2(graphics.GraphicsDevice.Viewport.Width / 2 - font.MeasureString("Single Player Setup Menu").X / 2, 125), Color.White);
+
+            //For each menu selection, if the current menu selection is selected draw it red, else draw it white.
+            for (int i = 0; i < numberOfSinglePlayerSetupMenuOptions; i++)
+            {
+                if (currentSinglePlayerSetupMenuOption == i)
+                {
+                    if (i == 0 && beginnerMode)
+                    {
+                        spriteBatch.DrawString(font, singlePlayerSetupMenuOptionsStrings[i] + " On", new Vector2(graphics.GraphicsDevice.Viewport.Width / 2 - font.MeasureString(singlePlayerSetupMenuOptionsStrings[i] + " On").X / 2, 150 + i * 25), Color.Red);
+                    }
+                    else if (i == 0 && !beginnerMode)
+                    {
+                        spriteBatch.DrawString(font, singlePlayerSetupMenuOptionsStrings[i] + " Off", new Vector2(graphics.GraphicsDevice.Viewport.Width / 2 - font.MeasureString(singlePlayerSetupMenuOptionsStrings[i] + " Off").X / 2, 150 + i * 25), Color.Red);
+                    }
+                    else if (i == 1 && randomMode)
+                    {
+                        spriteBatch.DrawString(font, singlePlayerSetupMenuOptionsStrings[i] + " On", new Vector2(graphics.GraphicsDevice.Viewport.Width / 2 - font.MeasureString(singlePlayerSetupMenuOptionsStrings[i] + " On").X / 2, 150 + i * 25), Color.Red);
+                    }
+                    else if (i == 1 && !randomMode)
+                    {
+                        spriteBatch.DrawString(font, singlePlayerSetupMenuOptionsStrings[i] + " Off", new Vector2(graphics.GraphicsDevice.Viewport.Width / 2 - font.MeasureString(singlePlayerSetupMenuOptionsStrings[i] + " Off").X / 2, 150 + i * 25), Color.Red);
+                    }
+                    else if (i == 3)
+                    {
+                        spriteBatch.DrawString(font, singlePlayerSetupMenuOptionsStrings[i] + numberOfAIPlayers, new Vector2(graphics.GraphicsDevice.Viewport.Width / 2 - font.MeasureString(singlePlayerSetupMenuOptionsStrings[i] + numberOfAIPlayers).X / 2, 150 + i * 25), Color.Red);
+                    }
+                    else
+                    {
+                        spriteBatch.DrawString(font, singlePlayerSetupMenuOptionsStrings[i], new Vector2(graphics.GraphicsDevice.Viewport.Width / 2 - font.MeasureString(singlePlayerSetupMenuOptionsStrings[i]).X / 2, 150 + i * 25), Color.Red);
+                    }
+                }
+                else
+                {
+                    if (i == 0 && beginnerMode)
+                    {
+                        spriteBatch.DrawString(font, singlePlayerSetupMenuOptionsStrings[i] + " On", new Vector2(graphics.GraphicsDevice.Viewport.Width / 2 - font.MeasureString(singlePlayerSetupMenuOptionsStrings[i] + " On").X / 2, 150 + i * 25), Color.White);
+                    }
+                    else if (i == 0 && !beginnerMode)
+                    {
+                        spriteBatch.DrawString(font, singlePlayerSetupMenuOptionsStrings[i] + " Off", new Vector2(graphics.GraphicsDevice.Viewport.Width / 2 - font.MeasureString(singlePlayerSetupMenuOptionsStrings[i] + " Off").X / 2, 150 + i * 25), Color.White);
+                    }
+                    else if (i == 1 && randomMode)
+                    {
+                        spriteBatch.DrawString(font, singlePlayerSetupMenuOptionsStrings[i] + " On", new Vector2(graphics.GraphicsDevice.Viewport.Width / 2 - font.MeasureString(singlePlayerSetupMenuOptionsStrings[i] + " On").X / 2, 150 + i * 25), Color.White);
+                    }
+                    else if (i == 1 && !randomMode)
+                    {
+                        spriteBatch.DrawString(font, singlePlayerSetupMenuOptionsStrings[i] + " Off", new Vector2(graphics.GraphicsDevice.Viewport.Width / 2 - font.MeasureString(singlePlayerSetupMenuOptionsStrings[i] + " Off").X / 2, 150 + i * 25), Color.White);
+                    }
+                    else if (i == 3)
+                    {
+                        spriteBatch.DrawString(font, singlePlayerSetupMenuOptionsStrings[i] + numberOfAIPlayers, new Vector2(graphics.GraphicsDevice.Viewport.Width / 2 - font.MeasureString(singlePlayerSetupMenuOptionsStrings[i] + numberOfAIPlayers).X / 2, 150 + i * 25), Color.White);
+                    }
+                    else
+                    {
+                        spriteBatch.DrawString(font, singlePlayerSetupMenuOptionsStrings[i], new Vector2(graphics.GraphicsDevice.Viewport.Width / 2 - font.MeasureString(singlePlayerSetupMenuOptionsStrings[i]).X / 2, 150 + i * 25), Color.White);
+                    }
+                }
+            }
+
+            spriteBatch.End();
         }
 
         private void drawMainMenu(GameTime gameTime)
@@ -193,6 +298,10 @@ namespace Tanto_Cuore
             {
                 handleMainMenuInput(keyboard, gamepad);
             }
+            else if(screenNumber == singlePlayerSetup)
+            {
+                handleSinglePlayerSetupMenuInput(keyboard, gamepad);
+            }
             else
             {
                 if ((keyboard.IsKeyDown(Keys.Up) && !keyboardOld.IsKeyDown(Keys.Up)) ||
@@ -220,6 +329,68 @@ namespace Tanto_Cuore
             gamepadOld = gamepad;
             mouseOld = mouse;
             keyboardOld = keyboard;
+        }
+
+        private void handleSinglePlayerSetupMenuInput(KeyboardState keyboard, GamePadState gamepad)
+        {
+            if ((keyboard.IsKeyDown(Keys.Up) && !keyboardOld.IsKeyDown(Keys.Up)) ||
+                    (ButtonState.Pressed == gamepad.DPad.Up && !(ButtonState.Pressed == gamepadOld.DPad.Up)) ||
+                    (keyboard.IsKeyDown(Keys.W) && !keyboardOld.IsKeyDown(Keys.W)) ||
+                    (gamepad.ThumbSticks.Left.Y > 0 && !(gamepadOld.ThumbSticks.Left.Y > 0)))
+            {
+                currentSinglePlayerSetupMenuOption--;
+                if (currentSinglePlayerSetupMenuOption < 0)
+                {
+                    currentSinglePlayerSetupMenuOption = numberOfSinglePlayerSetupMenuOptions - 1;
+                }
+            }
+
+            if ((keyboard.IsKeyDown(Keys.Down) && !keyboardOld.IsKeyDown(Keys.Down)) ||
+                (ButtonState.Pressed == gamepad.DPad.Down && !(ButtonState.Pressed == gamepadOld.DPad.Down)) ||
+                (keyboard.IsKeyDown(Keys.S) && !keyboardOld.IsKeyDown(Keys.S)) ||
+                (gamepad.ThumbSticks.Left.Y < 0 && !(gamepadOld.ThumbSticks.Left.Y < 0)))
+            {
+                currentSinglePlayerSetupMenuOption++;
+                if (currentSinglePlayerSetupMenuOption >= numberOfSinglePlayerSetupMenuOptions)
+                {
+                    currentSinglePlayerSetupMenuOption = 0;
+                }
+            }
+
+            if (keyboard.IsKeyDown(Keys.Enter) && !keyboardOld.IsKeyDown(Keys.Enter) ||
+                gamepad.IsButtonDown(Buttons.A) && !gamepadOld.IsButtonDown(Buttons.A))
+            {
+                if (currentSinglePlayerSetupMenuOption == 0)
+                {
+                    beginnerMode = !beginnerMode;
+                    randomMode = false;
+                }
+                else if (currentSinglePlayerSetupMenuOption == 1)
+                {
+                    beginnerMode = false;
+                    randomMode = !randomMode;
+                }
+                else if (currentSinglePlayerSetupMenuOption == 2)
+                {
+                    //to do
+                }
+                else if (currentSinglePlayerSetupMenuOption == 3)
+                {
+                    numberOfAIPlayers++;
+                    if (numberOfAIPlayers > 4){
+                        numberOfAIPlayers = 1;
+                    }
+                }
+                else if (currentSinglePlayerSetupMenuOption == 4)
+                {
+                    screenNumber = 4;
+                }
+
+                else if (currentSinglePlayerSetupMenuOption == 5)
+                {
+                    screenNumber = 0;
+                }
+            }
         }
     }
 }
